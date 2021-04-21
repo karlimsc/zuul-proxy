86ae9c027a6802dae767c50d98ff908e1ca2a672
karlimsc <karlianamsuarez@gmail.com>
2021-04-21 11:09:19 -0400

    zuul first version

--- src/main/java/com/project/zuulservice/ZuulserviceApplication.java
+++ src/main/java/com/project/zuulservice/ZuulserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -12,6 +13,26 @@ public class ZuulserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
