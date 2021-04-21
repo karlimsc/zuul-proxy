e44e82a5b0c0473bc6ea320be120707e04a8cdd6
karlimsc <karlianamsuarez@gmail.com>
2021-04-20 23:02:59 -0400

    first commit

---
+++ src/main/java/com/project/zuulservice/ZuulserviceApplication.java
@@ -0,0 +1,17 @@
+package com.project.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+
+}
